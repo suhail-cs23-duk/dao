3e96d28449551180b43691351d2b2debfb13f75a
Ananthan <ananthan.rajasekharan@gmail.com>
2024-11-12 15:06:33 +0530

    :zap: | deployment & ui

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # dao-1
+
+## Git Commands
+
+```
+ssh-keygen -t ed25519 -C "just a comment"
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/id_ed25519
+
+cat ~/.ssh/id_ed25519.pub
+```
+
+
+## UI Vite Commands
+```
+npm create vite@latest
+```
+```
+npm i @mui/material @mui/icons-material @emotion/styled @emotion/react
+```
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,17 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "localhost",
+  defaultNetwork: "sepolia",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
     hardhat: {
       // See its defaults
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/lHsk508FvXfDO0wO8DGxkumcQR_L_NhA",
+      accounts: ["private_key"] //0.01 Eth
     }
   },
   solidity: {
